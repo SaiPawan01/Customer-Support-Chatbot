36c2aff7587d6713ff40f77c9494b8722b14ed8b
Sai Pawan <porapupavansai.22.csm@anits.edu.in>
2026-03-30 12:12:35 +0530

    Protected chatbot page using protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import {Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage';
 import ChatbotInterface from './pages/ChatbotInterface';
+import ProtectedRoute from './components/ProtectedRoute';
 
 
 function App(){
@@ -12,7 +13,11 @@ function App(){
   <Routes>
     <Route path="/" element={<LandingPage />} />
     <Route path="/login" element={<LoginPage />} />
-    <Route path="/chatbot" element={<ChatbotInterface />} />
+    <Route path="/chatbot" element={
+      <ProtectedRoute>
+        <ChatbotInterface />
+      </ProtectedRoute>
+    } />
   </Routes>
   </>
 }
--- frontend/src/components/AuthPage/Oauth.jsx
+++ frontend/src/components/AuthPage/Oauth.jsx
@@ -15,5 +15,4 @@ export default function Oauth() {
               GitHub
             </button>
         </div >
-          
 }
